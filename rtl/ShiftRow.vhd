828227531a9b4ece4dda6ec0ce3a2e0a9b1ca1bf
EllaSeres <103682187+EllaSeres@users.noreply.github.com>
2024-04-06 14:31:52 +0300

    shift

--- rtl/ShiftRow.vhd
+++ rtl/ShiftRow.vhd
@@ -17,30 +17,21 @@ architecture rtl of ShiftRow is
        
 	    signal resultmatrix : matrix;
 		type ist_array8 is array (0 to 3) of std_logic_vector(7 downto 0);
-		signal inputrow: ist_array8;
-		signal outputrow:ist_array8;
-		type ist_array4 is array (0 to 3) of std_logic_vector(3 downto 0);
-		signal increment : ist_array4 :=(
-            "0000",
-            "0001",
-            "0010",
-            "0011"
-        );
 		
 		
    procedure ShiftInOneRow(
 								constant nr     : in std_logic_vector(3 downto 0);
-								signal onerow   : in ist_array8;
-								signal output   : inout ist_array8
+								variable onerow   : in ist_array8;
+								variable output   : inout ist_array8
 								) is
 								variable shift_amount : integer range 0 to 3 := to_integer(unsigned(nr));
 								
 	begin
 			 for i in 0 to 3 loop
 				if i + shift_amount >= 4 then
-					output(i) <= onerow(i + shift_amount - 4);
+					output(i) := onerow(i + shift_amount - 4);
 				else
-					output(i) <= onerow(i + shift_amount);
+					output(i) := onerow(i + shift_amount);
 					
 				end if;
 			end loop;
@@ -50,6 +41,16 @@ architecture rtl of ShiftRow is
 begin
 
     process(clk) 
+	    variable inputrow: ist_array8;
+		variable outputrow:ist_array8;
+		type ist_array4 is array (0 to 3) of std_logic_vector(3 downto 0);
+		variable increment : ist_array4 :=(
+            "0000",
+            "0001",
+            "0010",
+            "0011"
+        );
+		
     begin 
         if rising_edge(Clk) then
             if reset = '0' then
@@ -59,11 +60,11 @@ begin
 			for i in 0 to 3 loop
 			    
 				for j in 0 to 3 loop
-					inputrow(j)<=inputmatrix(i,j);
+					inputrow(j):=inputmatrix(i,j);
 				end loop;
 				
 				if i = 0 then
-				     outputrow <= inputrow;
+				     outputrow := inputrow;
 				else
 					ShiftInOneRow(increment(i),inputrow,outputrow);
 				end if;
